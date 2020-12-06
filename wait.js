5088032b33b3c76c2c588d3061df7a6bb5eb3981
brian <riceb53@gmail.com>
2020-12-06 13:20:53 -0500

    async stuff

---
+++ wait.js
@@ -0,0 +1,6 @@
+setTimeout(function() {
+  console.log('starting the first task');
+}, 2000);
+
+
+console.log('completed first task');
---
+++ wait.rb
@@ -0,0 +1,4 @@
+sleep 2
+
+p "starting the first task"
+p "done with the first task"
