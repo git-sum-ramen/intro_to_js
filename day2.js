349c0126c57ca947a5d3ecbddaabe08402a0f7b8
brian <riceb53@gmail.com>
2020-12-06 11:33:50 -0500

    descendingsolution

--- day2.js
+++ day2.js
@@ -28,14 +28,28 @@ function selectEvenItems(strings) {
 
 
 
-function factorial(number) {
-  var result = 1;
-  var currentNumber = number;
-  for (var i = 0; i < number; i++) {
-    result = result * currentNumber;
-    currentNumber--;
+// function factorial(number) {
+//   var result = 1;
+//   for (var i = 0; i < number; i++) {
+//     // console.log(i + 1)
+//     result = result * (i + 1);    
+//   }
+//   return result;
+// }
+
+// console.log(factorial(5));
+
+
+
+function descending(numbers) {
+  var result = [];
+  var index = numbers.length - 1;
+  for (var i = 0; i < numbers.length; i++) {
+    console.log(i);
+    result.push(numbers[index]);
+    index--;
   }
   return result;
 }
 
-console.log(factorial(5));
+console.log(descending([1, 3, 5, 7]));
