537ebaf0a716f6e5df3c153b7d55f74128f3aa6e
brian <riceb53@gmail.com>
2020-12-06 13:10:00 -0500

    day 2 demo 2

--- day2.js
+++ day2.js
@@ -64,4 +64,65 @@ function sumCombinations(numbers1, numbers2) {
   return result;
 }
 
-console.log(sumCombinations([1, 5, 10], [100, 500, 1000]));
+// console.log(sumCombinations([1, 5, 10], [100, 500, 1000]));
+
+
+
+// PART 2
+
+
+// 8. Write a function that takes in an array of numbers and returns a count of how many zeros are in the array.
+
+function countOfZeroes(numbers) {
+  // console.log(numbers);
+  var count = 0;
+  numbers.forEach(function(number) {
+    if (number === 0) {
+      count++;
+    }
+  })
+  return count;
+}
+
+// console.log(countOfZeroes([0,13,6,2,2,5,2,0,12,4,0,123,0]));
+
+// 9. Write a function that takes in an array of numbers and returns true if all the numbers are bigger than 10, otherwise returns false.
+
+function allGreaterThanTen(numbers) {
+  var greaterThanTen = true;
+  numbers.forEach(function(number) {
+    if (number <= 10) {
+      greaterThanTen = false;
+    }
+    // if the number > 10, keep going
+    // if it's not, then return false
+  })
+  return greaterThanTen;
+}
+
+// console.log(allGreaterThanTen([12,14,16,18]));
+// console.log(allGreaterThanTen([6, 12,14,16,18, 16]));
+
+// 10. Write a function that takes in an array of words and returns the number of times the letter “a” appeared in total.
+
+function totalAs(words) {
+  // console.log(words);
+  var i = 0;
+  var count = 0;
+  while (i < words.length) {
+    var j = 0;
+    while (j < words[i].length) {
+      var letter = words[i][j];
+      // console.log(letter);
+      if (letter === 'a') {
+        count++;
+      }
+      j++;
+    }
+    // console.log(words[i]);
+    i++;
+  }
+  return count
+}
+
+console.log(totalAs(["apple", "orange", "grapes", "pineapple"]));
